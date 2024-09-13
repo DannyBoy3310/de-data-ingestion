7eb997ee640934aa1def8e32ed3eccd191ec99f3
DannyBoy3310 <ranjithm.17mtr@gmail.com>
2024-09-13 23:30:40 +0530

    Added log

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     arguments = load_arguments()
     logging.info(f"Creating Spark Session for {arguments.entity_name}")
     spark = create_spark_session(arguments)
-
     dataframe = read_dataframe_as_csv(spark,arguments)
     dataframe.show(5, truncate = False)
+    logging.info("Closing Spark Session")
 
